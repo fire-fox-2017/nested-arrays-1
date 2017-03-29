4df33b02fa7ab4de286d3dff8dd0e9084912dbf6
Bill Ivan <billivank_96@yahoo.co.id>
2017-03-29 16:22:57 +0700

    selesai

--- chessboard.js
+++ chessboard.js
@@ -24,14 +24,7 @@ const makeChessboard = () => {
 }
 
 const printBoard = x => {
-  console.log(x[0]);
-  console.log(x[1]);
-  console.log(x[2]);
-  console.log(x[3]);
-  console.log(x[4]);
-  console.log(x[5]);
-  console.log(x[6]);
-  console.log(x[7]);
+  console.log(x);
 }
 
 printBoard(makeChessboard())
