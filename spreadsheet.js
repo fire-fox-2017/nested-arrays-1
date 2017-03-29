a4fe19a2773796b2013f656f80f641551732a0b5
Didit Aditya <email": null>
2017-03-29 11:56:46 +0700

    Update my upload

--- spreadsheet.js
+++ spreadsheet.js
@@ -16,16 +16,20 @@ class Spreadsheet {
 
 var table = new Spreadsheet();
 
-let data1 = ["12", "Joe Schmo", "Center", "[14,32,7,0,23]"];
-let data2 = ["9", "Ken Buckets", "Point Guard", "[19,0,11,22,0]"];
-let data3 = ["31", "Harvey Kay", "Shooting Guard", "[0,30,16,0,25]"];
+let data1 = ["12", "Joe Schmo", "Center", [14,32,7,0,23]];
+let data2 = ["9", "Ken Buckets", "Point Guard", [19,0,11,22,0]];
+let data3 = ["31", "Harvey Kay", "Shooting Guard", [0,30,16,0,25]];
+//etc
+
 table.addData(data1);
 table.addData(data2);
 table.addData(data3);
+//etc
+
 table.getData();
 
 console.log(table._spreadsheet[1][3]);
 console.log(table._spreadsheet[2][2]);
-
+console.log(JSON.stringify(table._spreadsheet[1][3]) === JSON.stringify([14,32,7,0,23]));
 
 module.exports = Spreadsheet
