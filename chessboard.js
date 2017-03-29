883d01869ab62d5b43981ac599ab5fa15919cdc4
Daniel Sidabutar <email": null>
2017-03-29 11:36:43 +0700

    Upload Chessboard and Spreadsheet

--- chessboard.js
+++ chessboard.js
@@ -4,11 +4,48 @@ const makeChessboard = () => {
   let chessboard = []
 
   // ... write your code here
-  return chessboard
+  let row = 8;
+  let col = 8;
+
+  let strBackRowSet = "Rook Knight Bishop King Queen Bishop Knight Rook";
+  let backRowSet = strBackRowSet.split(" ");
+
+  let frontRowSet = [];
+  for (let i = 0; i < row; i++) {
+    frontRowSet.push("Pawn");
+  }
+
+  let other = [];
+  for (let i = 0; i < row; i++) {
+    other.push("Empty");
+  }
+
+  let colors = ["White", "Black"];
+
+  for (var i = 0; i < row; i++) {
+    chessboard.push([]);
+  }
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (i === 0) {
+        chessboard[i].push(colors[0] + " " + backRowSet[j]);
+      } else if (i === 1) {
+        chessboard[i].push(colors[0] + " " + frontRowSet[j]);
+      } else if (i === row-1) {
+        chessboard[i].push(colors[1] + " " + backRowSet[j]);
+      } else if (i === row-2) {
+        chessboard[i].push(colors[1] + " " + frontRowSet[j]);
+      }
+    }
+  }
+
+  return chessboard;
 }
 
 const printBoard = x => {
   // ... write your code here
+  console.log(x);
 }
 
 printBoard(makeChessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -4,11 +4,28 @@ class Spreadsheet {
     this._spreadsheet = [
       ['Number', 'Name', 'Position', 'Points per Game']
     ]
+
   }
   addData (data) {
+    this._spreadsheet.push(data);
   }
   getData () {
+    console.log(this._spreadsheet);
   }
 }
+
+var table = new Spreadsheet();
+
+let data1 = ["12", "Joe Schmo", "Center", "[14,32,7,0,23]"];
+let data2 = ["9", "Ken Buckets", "Point Guard", "[19,0,11,22,0]"];
+let data3 = ["31", "Harvey Kay", "Shooting Guard", "[0,30,16,0,25]"];
+table.addData(data1);
+table.addData(data2);
+table.addData(data3);
+table.getData();
+
+console.log(table._spreadsheet[1][3]);
+console.log(table._spreadsheet[2][2]);
+
 
 module.exports = Spreadsheet
