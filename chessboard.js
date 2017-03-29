dc4e50151f6375c5046f34d403bae77d643e0f4b
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2017-03-29 21:58:17 +0700

    Nested Array 1 Selesai

--- chessboard.js
+++ chessboard.js
@@ -4,63 +4,26 @@ const makeChessboard = () => {
 
   // ... write your code here
 
-  let chessboard = [];
-  let bidakCatur = ['Benteng', 'Kuda', 'Gajah', 'Menteri', 'Raja', 'Pion',' '];
+  var chessboard = [];
+  var bidakCatur = ['Benteng', 'Kuda', 'Gajah', 'Menteri', 'Raja', 'Gajah', 'Kuda', 'Benteng'];
+  var pionCatur = ['Pion'];
 
   for (var i = 0; i < 8; i++) {
     chessboard.push([]);
   }
 
-  for (let baris = 0; baris < 8; baris++) {
-    for (let kolom = 0; kolom < 8; kolom++) {
+  for (var baris = 0; baris < chessboard.length; baris++) {
+    for(var kolom = 0; kolom < chessboard.length; kolom++){
       if (baris === 0) {
-        if (kolom === 0 || kolom === 7) {
-          chessboard[0].push(bidakCatur[0] + ' Hitam');
-        } else if (kolom === 1 || kolom === 6) {
-          chessboard[0].push(bidakCatur[1] + ' Hitam');
-        } else if (kolom === 2 || kolom === 5) {
-          chessboard[0].push(bidakCatur[2] + ' Hitam');
-        } else if (kolom === 3) {
-          chessboard[0].push(bidakCatur[3] + ' Hitam');
-        } else if (kolom === 4) {
-          chessboard[0].push(bidakCatur[4] + ' Hitam');
-        }
-      }
-
-      if (baris === 1) {
-        chessboard[1].push(bidakCatur[5] + ' Hitam');
-      }
-
-      if (baris === 2){
-        chessboard[2].push(bidakCatur[6]);
-      }
-      if (baris === 3){
-        chessboard[3].push(bidakCatur[6]);
-      }
-      if (baris === 4){
-        chessboard[4].push(bidakCatur[6]);
-      }
-
-      if (baris === 5) {
-        chessboard[5].push(bidakCatur[6]);
-      }
-
-      if (baris === 6) {
-        chessboard[6].push(bidakCatur[6]);
-      }
-
-      if (baris === 7) {
-        if (kolom === 0 || kolom === 7) {
-          chessboard[7].push(bidakCatur[0] + ' Putih');
-        } else if (kolom === 1 || kolom === 6) {
-          chessboard[7].push(bidakCatur[1] + ' Putih');
-        } else if (kolom === 2 || kolom === 5) {
-          chessboard[7].push(bidakCatur[2] + ' Putih');
-        } else if (kolom === 3) {
-          chessboard[7].push(bidakCatur[3] + ' Putih');
-        } else if (kolom === 4) {
-          chessboard[7].push(bidakCatur[4] + ' Putih');
-        }
+        chessboard[baris].push(bidakCatur[kolom] + ' Hitam');
+      } else if (baris === 1){
+        chessboard[baris].push(pionCatur[0] + ' Hitam');
+      } else if (baris === 6){
+        chessboard[baris].push(pionCatur[0] + ' Putih');
+      } else if (baris === 7){
+        chessboard[baris].push(bidakCatur[kolom] + ' Putih');
+      } else {
+        chessboard[baris].push(' ');
       }
     }
   }
