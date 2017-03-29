6934e2c141eb1b3ddd33f4d6952ace39f1a4ed45
Bhirawa Mbani <bhirmbani@gmail.com>
2017-03-29 10:50:41 +0700

    nested-arrays-1 first done

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,34 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
 
-  // ... write your code here
-  return chessboard
+  let chessboard = []
+  let mainPiece = ['Benteng', 'Kuda', 'Menteri', 'Raja', 'Ratu', 'Menteri', 'Kuda', 'Benteng'];
+  let piece = ['Pion', 'Pion', 'Pion', 'Pion', 'Pion', 'Pion', 'Pion', 'Pion'];
+
+  for(let i = 0; i < mainPiece.length; i++) {
+    chessboard.push([]);
+    for(let j = 0; j < mainPiece.length; j++) {
+      if(i === 0) {
+        chessboard[i].push(`${mainPiece[j]} Hitam`);
+      } else if (i === 1) {
+        chessboard[i].push(`${piece[j]} Hitam`);
+      } else if (i === 6) {
+        chessboard[i].push(`${piece[j]} Putih`);
+      } else if (i === 7) {
+        chessboard[i].push(`${mainPiece[j]} Putih`);
+      } else {
+        chessboard[i].push('');
+      }
+    }
+  }
+
+  return chessboard;
 }
 
 const printBoard = x => {
   // ... write your code here
+  console.log(x);
 }
 
 printBoard(makeChessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,14 +1,36 @@
 // ... write your code here
 class Spreadsheet {
   constructor () {
-    this._spreadsheet = [
-      ['Number', 'Name', 'Position', 'Points per Game']
-    ]
+    this.spreadsheet = [
+      ['Number', 'Name', 'Position', 'Points per Game'],
+    ],
+    this.table = [];
   }
   addData (data) {
+    this.spreadsheet.push(data);
   }
   getData () {
+    return this.spreadsheet;
   }
 }
+
+let sheet = new Spreadsheet();
+
+let data1 = [ '12', 'Joe Schmo', 'Center', [ 14, 32, 7, 0, 23 ] ];
+let data2 =  ['9', 'Ken Bucktes', 'Point Guard', [19,0,11,22,0]];
+let data3 = ['31', 'Harvey Kay', 'Shooting Guard', [0,30,16,0,25]];
+let data4 = ['18', 'Sally Talls', 'Power Forward', [18,29,26,31,19]];
+let data5 = ['22', 'Mo DiBoux', 'Small Forward', [11,0,23,7,0]];
+
+sheet.addData(data1);
+sheet.addData(data2);
+sheet.addData(data3);
+sheet.addData(data4);
+sheet.addData(data5);
+
+let table = sheet.getData();
+
+console.log(table[3][2] == "Shooting Guard");
+console.log(JSON.stringify(table[1][3]) == JSON.stringify([14, 32, 7, 0, 23]));
 
 module.exports = Spreadsheet
