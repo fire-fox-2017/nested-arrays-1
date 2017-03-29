6b90451493250d332e58f4e7ecf8ebfe6832f420
Matt Lee <email": null>
2017-03-29 15:00:09 +0700

    nested array 1 mattlcy

--- chessboard.js
+++ chessboard.js
@@ -2,13 +2,23 @@
 
 const makeChessboard = () => {
   let chessboard = []
+  let bPieces = ['Black Rook','Black Knight','Black Bishop','Black Queen','Black King','Black Bishop','Black Knight','Black Rook']
+  let bPawn = ['Black Pawn','Black Pawn','Black Pawn','Black Pawn','Black Pawn','Black Pawn','Black Pawn','Black Pawn']
+  let wPieces = ['White Rook','White Knight','White Bishop','White Queen','White King','White Bishop','White Knight','White Rook']
+  let wPawn = ['White Pawn','White Pawn','White Pawn','White Pawn','White Pawn','White Pawn','White Pawn','White Pawn']
 
-  // ... write your code here
+  for (var a=0; a<8; a++) {
+    chessboard[a] = []
+  }
+  chessboard[0] = bPieces
+  chessboard[1] = bPawn
+  chessboard[6] = wPawn
+  chessboard[7] = wPieces
   return chessboard
 }
 
 const printBoard = x => {
-  // ... write your code here
+  return makeChessboard
 }
 
 printBoard(makeChessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -6,9 +6,21 @@ class Spreadsheet {
     ]
   }
   addData (data) {
+    this._spreadsheet.push(data)
+    // return this._spreadsheet
   }
   getData () {
   }
 }
 
+let ss = new Spreadsheet()
+ss.addData(['12','Joe Schmo','Center','[14,32,7,0,23]']);
+ss.addData(['9','Ken Buckets','Point Guard','[19,0,11,22,0]']);
+ss.addData(['31','Harvey Kay','Shooting Guard','[0,36,16,0,25]']);
+ss.addData(['18','Sally Talls','Power Forward','[18,29,26,31,19]']);
+ss.addData(['22','Mo DiBoux','Small Forward','[11,0,23,17,0]']);
+console.log(ss)
+let table = ss._spreadsheet
+console.log(table[3][2])
+console.log(JSON.stringify(table[1][3]) == JSON.stringify('[14,32,7,0,23]'));
 module.exports = Spreadsheet
