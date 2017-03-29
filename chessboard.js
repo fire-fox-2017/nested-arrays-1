200c170d42ff6c422ba2b21e9131e2b1807ef64a
dyankastutara <dyankastutara19@gmail.com>
2017-03-29 12:00:42 +0700

    Pertama Comm

--- chessboard.js
+++ chessboard.js
@@ -4,11 +4,30 @@ const makeChessboard = () => {
   let chessboard = []
 
   // ... write your code here
+  let chess = ["Benteng","Kuda","Peluncur","Menteri","Raja","Peluncur","Kuda","Benteng"];
+  let pawn = ["Pion","Pion","Pion","Pion","Pion","Pion","Pion","Pion"];
+
+  for(var i=0;i<8;i++){
+    chessboard.push([]);
+    for(var j=0;j<8;j++){
+      if(i===0){
+        chessboard[i].push(chess[j]+" Hitam");
+      }else if(i===1){
+        chessboard[i].push(pawn[j]+" Hitam");
+      }else if(i===6){
+        chessboard[i].push(pawn[j]+" Putih");
+      }else if(i===7){
+        chessboard[i].push(chess[j]+" Putih");
+      }
+    }
+  }
+
   return chessboard
 }
 
 const printBoard = x => {
   // ... write your code here
+  console.log(x[0][1]);
 }
 
 printBoard(makeChessboard())
