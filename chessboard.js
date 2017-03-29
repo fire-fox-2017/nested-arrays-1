0d45b54ab1278c265989f2efac13bdc1cf0dd1e3
raeseuky <raeseukypinto@gmail.com>
2017-03-29 20:40:23 +0700

    week1-day3-1

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,67 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
+  let chessboard = [];
+  
+
 
   // ... write your code here
+  var jabatan = ['benteng', 'kuda', 'menteri', 'ratu', 'raja', 'menteri','kuda','benteng'];
+  var pion = ['pion', 'pion','pion','pion','pion','pion','pion','pion']
+  var warna = ['hitam','putih'];
+  var posisijabatanhitam=[];
+  var posisijabatanputih=[];
+  var posisipionhitam=[];
+  var posisipionputih=[];
+  var papan=['','','','','','','',''];
+   for (var i=0; i<jabatan.length; i++){
+
+   	posisijabatanhitam.push(jabatan[i]+ " " +warna[0]);
+   	posisijabatanputih.push(jabatan[i]+ " " +warna[1]);
+   	posisipionhitam.push(pion[i]+" "+warna[0]);
+   	posisipionputih.push(pion[i]+" "+warna[1]);
+   	
+   }
+   for ( var a=0; a<=papan.length-1; a++){
+
+   }
+   
+
+   for (var b=0; b<=7; b++){
+    if (b===0){
+      chessboard.push(posisijabatanhitam);
+    }
+    else if (b===1){
+      chessboard.push(posisipionhitam);
+    }
+    else if (b===2){
+      chessboard.push(papan);
+    }
+    else if (b===3){
+      chessboard.push(papan);
+    }
+    else if (b===4){
+      chessboard.push(papan);
+    }
+    else if (b===5){
+      chessboard.push(papan);
+    }
+    else if (b===6){
+      chessboard.push(posisipionputih);
+    }
+    else if (b===7){
+      chessboard.push(posisijabatanputih);
+    }
+   }
+  
+
+
   return chessboard
 }
 
 const printBoard = x => {
   // ... write your code here
+  console.log(x);
 }
 
 printBoard(makeChessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,14 +1,39 @@
+'use strict'
+
 // ... write your code here
+var file = ['12','joe Schmo','center',['14','32','7','0','23']];
+var file1 = ['9','ken buckets','point guard',['19','0','11','22','0']];
+var file2 = ['31','harvey kay','shooting guard',['0','30','16','0','25']];
+var file3 = ['18','sally talls','power forward',['18','29','26','31','19']];
+var file4 = ['22','mo diboux','small forward',['11','0','23','17','0']];
+
 class Spreadsheet {
   constructor () {
     this._spreadsheet = [
       ['Number', 'Name', 'Position', 'Points per Game']
     ]
+
   }
   addData (data) {
+ this._spreadsheet.push(data);
+
   }
   getData () {
+  	 return this._spreadsheet;
   }
+
+
 }
+let newData = new Spreadsheet();
+
+newData.addData(file);
+newData.addData(file1);
+newData.addData(file2);
+newData.addData(file3);
+newData.addData(file4);
+
+console.log(newData.getData());
+
 
 module.exports = Spreadsheet
+
