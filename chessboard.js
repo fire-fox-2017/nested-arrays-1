6c0ce6c0eabc05039f087ddd16439d7ee1883edc
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-03-29 12:11:58 +0700

    mengerjakan tugas array-1

--- chessboard.js
+++ chessboard.js
@@ -1,17 +1,36 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
-
+  let chessboard = [];
   // ... write your code here
-  return chessboard
+  var chessColor = ['black', 'white'];
+  var chessKingdom = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'];
+  var chessPion = ['pion', 'pion', 'pion', 'pion', 'pion', 'pion', 'pion', 'pion'];
+  var chessKingdomWhite = [];
+  var chessKingdomBlack = [];
+  var pionWhite = [];
+  var pionBlack = [];
+  for (var i=0; i<chessKingdom.length; i++){
+    chessKingdomWhite.push(chessColor[1] + " " + chessKingdom[i]);
+    chessKingdomBlack.push(chessColor[0] + " " + chessKingdom[i]);
+    pionWhite.push(chessColor[1] + " " + chessPion[i]);
+    pionBlack.push(chessColor[0] + " " + chessPion[i]);
+  }
+  chessboard.push(chessKingdomBlack);
+  chessboard.push(pionBlack);
+  chessboard.push(pionWhite);
+  chessboard.push(chessKingdomWhite);
+
+  chessboard.splice(2, 0, [], [], [], []);
+  return chessboard;
 }
 
 const printBoard = x => {
   // ... write your code here
+  console.log(x);
 }
 
-printBoard(makeChessboard())
+printBoard(makeChessboard());
 
 module.exports = {
   makeChessboard,
