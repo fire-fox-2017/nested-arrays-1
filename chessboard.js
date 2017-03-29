b7c1df5661ea35e2277fe0b042a54d32d76c21f9
fearzen3 <ivanhabi2@gmail.com>
2017-03-29 12:00:08 +0700

    selse

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,58 @@
 'use strict'
 
-const makeChessboard = () => {
-  let chessboard = []
+function susun(indexRow){
+  let pasukan = ['pion','benteng','kuda','mentri','raja','permaisuri'];
+  let w='';
+  let susunan = [];
 
-  // ... write your code here
-  return chessboard
+
+  if(indexRow==0||indexRow==7){
+    if(indexRow==0){
+      w=' hitam';
+    }else{
+      w=' putih';
+    }
+    for(let i=1;i<pasukan.length;i++){
+      susunan.push(pasukan[i]+w);
+    }
+    for(let i=pasukan.length-3;i>=1;i--){
+      susunan.push(pasukan[i]+w);
+    }
+  } else if(indexRow==1||indexRow==6){
+    if(indexRow==1){
+      w=' hitam';
+    }else{
+      w=' putih';
+    }
+    for(let i=0;i<8;i++){
+      susunan.push(pasukan[0]+w);
+    }
+  } else{
+    for(let i=0;i<8;i++){
+      susunan.push(" ");
+    }
+
+
+  }
+
+  return susunan;
+}
+
+const makeChessboard = () => {
+  let chessboard = [];
+
+  for(let i=0;i<8;i++){
+    chessboard.push(susun(i));
+  }
+
+
+
+  return chessboard;
+
 }
 
 const printBoard = x => {
-  // ... write your code here
+  console.log(x);
 }
 
 printBoard(makeChessboard())
