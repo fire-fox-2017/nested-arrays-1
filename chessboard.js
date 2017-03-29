898be4ff879cf6957abf4f2f8ce362623d85fd24
Bill Ivan <billivank_96@yahoo.co.id>
2017-03-29 11:13:38 +0700

    nested array 1 selesai

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,30 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
-
-  // ... write your code here
+  let chessboard = [];
+  var pie = ["rook", "knight", "bishop", "king", "queen", "bishop", "knight", "rook"];
+  var col = ["black", "white"];
+  var paw = "pawn";
+  for (var a = 0; a<8; a++){
+    chessboard.push([]);
+  }
+  for (var b=0; b<8; b++){
+    chessboard[1].push(col[0]+" "+paw);
+    chessboard[2].push("");
+    chessboard[3].push("");
+    chessboard[4].push("");
+    chessboard[5].push("");
+    chessboard[6].push(col[1]+" "+paw);
+  }
+  for (var i = 0; i<pie.length; i++){
+    chessboard[0].push(col[0]+" "+pie[i]);
+    chessboard[7].push(col[1]+" "+pie[i]);
+  }
   return chessboard
 }
 
 const printBoard = x => {
-  // ... write your code here
+  console.log(makeChessboard());
 }
 
 printBoard(makeChessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,14 +1,20 @@
 // ... write your code here
-class Spreadsheet {
-  constructor () {
-    this._spreadsheet = [
-      ['Number', 'Name', 'Position', 'Points per Game']
-    ]
-  }
-  addData (data) {
-  }
-  getData () {
-  }
+
+
+var _spreadsheet = [
+  ['Number', 'Name', 'Position', 'Points per Game']
+]
+
+function addData (data) {
+  _spreadsheet.push(data)
+}
+function getData () {
+  console.log(_spreadsheet);
 }
 
-module.exports = Spreadsheet
+addData([12,"Joe Schmo","Center",[14,32,7,0,23]]);
+addData([9,"Ken Buckets","Point Guard",[19,0,11,12,0]]);
+addData([31,"Harvey Kay", "Shooting Guard",[0,30,16,0,25]]);
+addData([18,"Sally Talls","Power Forward",[18,29,26,31,19]]);
+addData([22,"Mo DiBoux","Small Forward",[11,0,23,17,0]]);
+getData();
