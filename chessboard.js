3978372ee940c11e739b041626d58b37e565eae2
stedyyulius <stedyyulius@gmail.com>
2017-03-29 20:25:45 +0700

    first commit

--- chessboard.js
+++ chessboard.js
@@ -1,11 +1,18 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
+  let chessboard = [];
+  var caturhitam = ["Benteng Hitam", "Kuda Hitam", "Luncur Hitam", "Ster Hitam", "Raja Hitam","Luncur Hitam","Kuda Hitam","Benteng Hitam"];
+  var pionputih = ["Pion Putih","Pion Putih","Pion Putih","Pion Putih","Pion Putih","Pion Putih","Pion Putih","Pion Putih"];
+  var pionhitam = ["Pion Hitam","Pion Hitam","Pion Hitam","Pion Hitam","Pion Hitam","Pion Hitam","Pion Hitam","Pion Hitam"];
+  var caturputih = ["Benteng Putih", "Kuda Putih", "Luncur Putih", "Ster Putih", "Raja Putih","Luncur Putih","Kuda Putih","Benteng Putih"];
+  var empty = [" "," "," "," "," "," "," "," "];
+  chessboard.push(caturputih,pionputih,empty,empty,pionhitam,caturhitam);
 
-  // ... write your code here
-  return chessboard
+console.log(chessboard);
 }
+makeChessboard();
+
 
 const printBoard = x => {
   // ... write your code here
