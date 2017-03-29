017fe7c2496affac4370b0502bebbf8fb04fc2c2
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2017-03-29 11:55:48 +0700

    Nested Array 1 Selesai

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,63 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
 
   // ... write your code here
-  return chessboard
+
+  let chessboard = [];
+  let bidakCatur = ['Benteng', 'Kuda', 'Gajah', 'Menteri', 'Raja', 'Pion'];
+
+  for (var i = 0; i < 8; i++) {
+    chessboard.push([]);
+  }
+
+  for (let baris = 0; baris < 8; baris++) {
+    for (let kolom = 0; kolom < 8; kolom++) {
+      if (baris === 0) {
+        if (kolom === 0 || kolom === 7) {
+          chessboard[0].push(bidakCatur[0] + ' Hitam');
+        } else if (kolom === 1 || kolom === 6) {
+          chessboard[0].push(bidakCatur[1] + ' Hitam');
+        } else if (kolom === 2 || kolom === 5) {
+          chessboard[0].push(bidakCatur[2] + ' Hitam');
+        } else if (kolom === 3) {
+          chessboard[0].push(bidakCatur[3] + ' Hitam');
+        } else if (kolom === 4) {
+          chessboard[0].push(bidakCatur[4] + ' Hitam');
+        }
+      }
+
+      if (baris === 1) {
+        chessboard[1].push(bidakCatur[5] + ' Hitam');
+      }
+
+      if (baris === 6) {
+        chessboard[6].push(bidakCatur[5] + ' Putih');
+      }
+
+      if (baris === 7) {
+        if (kolom === 0 || kolom === 7) {
+          chessboard[7].push(bidakCatur[0] + ' Putih');
+        } else if (kolom === 1 || kolom === 6) {
+          chessboard[7].push(bidakCatur[1] + ' Putih');
+        } else if (kolom === 2 || kolom === 5) {
+          chessboard[7].push(bidakCatur[2] + ' Putih');
+        } else if (kolom === 3) {
+          chessboard[7].push(bidakCatur[3] + ' Putih');
+        } else if (kolom === 4) {
+          chessboard[7].push(bidakCatur[4] + ' Putih');
+        }
+      }
+    }
+  }
+
+  return chessboard;
+
 }
 
 const printBoard = x => {
   // ... write your code here
+  console.log(x);
 }
 
 printBoard(makeChessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,14 +1,33 @@
 // ... write your code here
 class Spreadsheet {
-  constructor () {
+  constructor() {
     this._spreadsheet = [
       ['Number', 'Name', 'Position', 'Points per Game']
     ]
   }
-  addData (data) {
+  addData(data) {
+
+    this._spreadsheet.push(data);
   }
-  getData () {
+  getData() {
+    console.log(this._spreadsheet);
   }
 }
 
-module.exports = Spreadsheet
+let objSpreadsheet = new Spreadsheet;
+
+let data1 = ['12','Joe Schmo','Center',[14,32,7,0,23]];
+let data2 = ['9','Ken Buckets','Point Guard',[19,0,11,22,0]];
+let data3 = ['31','Harvey Kay','Shooting Guard',[0,30,16,0,25]];
+let data4 = ['18','Sally Talls','Power Forward',[18,29,26,31,19]];
+let data5 = ['22','Mo Diboux','Small Forward',[11,0,23,17,0]];
+
+objSpreadsheet.addData(data1);
+objSpreadsheet.addData(data2);
+objSpreadsheet.addData(data3);
+objSpreadsheet.addData(data4);
+objSpreadsheet.addData(data5);
+objSpreadsheet.getData();
+
+
+// module.exports = Spreadsheet
