264c48e3ae57abfa73888f36693efd8773dde28c
Parel Wellman Hutahaean <parel.hutahaean@gmail.com>
2017-03-29 16:56:16 +1400

    Release 1: Data Table Finished

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,14 +1,29 @@
 // ... write your code here
-class Spreadsheet {
-  constructor () {
-    this._spreadsheet = [
-      ['Number', 'Name', 'Position', 'Points per Game']
-    ]
-  }
-  addData (data) {
-  }
-  getData () {
+function Spreadsheet() {
+  this.spreadsheet = [
+    ['Number', 'Name', 'Position', 'Points per Game']
+  ],
+  this.addData = data => {
+    this.spreadsheet.push(data);
+  },
+  this.getData = () => {
+    return this.spreadsheet;
   }
 }
+
+let table = new Spreadsheet();
+let data = ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]];
+table.addData(data);
+data = ["9", "Ken Buckets", "Point Guard", [19, 0, 11, 22, 0]];
+table.addData(data);
+data = ["31", "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]];
+table.addData(data);
+data = ["18", "Sally Talls", "Power Forward", [18, 29, 26, 31, 19]];
+table.addData(data);
+data = ["22", "Mo DiBoux", "Small Forward", [11, 0, 23, 17, 0]];
+table.addData(data);
+console.log(table.getData());
+console.log(table.getData()[3][2] == "Shooting Guard");
+console.log(JSON.stringify(table.getData()[1][3]) == JSON.stringify([14, 32, 7, 0, 23]));
 
 module.exports = Spreadsheet
