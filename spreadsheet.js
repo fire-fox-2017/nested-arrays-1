5fabaa4d2076d29c2d90bac9d312cc20aab96c5b
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-03-29 14:02:39 +0700

    mengerjakan spreadsheet deri kurniawan

--- spreadsheet.js
+++ spreadsheet.js
@@ -6,9 +6,23 @@ class Spreadsheet {
     ]
   }
   addData (data) {
+    this._spreadsheet.push(data);
   }
   getData () {
+    console.log(this._spreadsheet);
   }
 }
 
-module.exports = Spreadsheet
+let dataTable = new Spreadsheet;
+let data1 = [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23]];
+dataTable.addData(data1);
+let data2 = [9, 'Ken Buckets', 'Point Guard', [19, 0, 11, 22, 0]];
+dataTable.addData(data2);
+let data3 = [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25]];
+dataTable.addData(data3);
+let data4 = [18, 'Sally Talls', 'Power Forward', [18, 29, 26, 31, 19]];
+dataTable.addData(data4);
+
+dataTable.getData();
+
+module.exports = Spreadsheet;
