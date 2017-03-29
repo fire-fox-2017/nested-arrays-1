37ba13f02e68c18d5c28d92c3c885c8916bfcc32
haryanawisnu <arya.wisnu05@gmail.com>
2017-03-29 10:35:02 +0700

    done

--- chessboard.js
+++ chessboard.js
@@ -4,11 +4,30 @@ const makeChessboard = () => {
   let chessboard = []
 
   // ... write your code here
+  var arrTroops=["Benteng","Kuda","Gajah","Menteri","Raja","Gajah","Kuda","Benteng"];
+  for (var i = 0; i < 8; i++) {
+  	var arrTemp=[];
+  	for (var j = 0; j < 8; j++) {
+  		if(i==0){
+  			arrTemp.push(arrTroops[j]+" Hitam");
+  		}else if(i==1){
+  			arrTemp.push("Budak Hitam");
+  		}else if(i==6){
+  			arrTemp.push("Budak Putih");
+  		}else if(i==7){
+  			arrTemp.push(arrTroops[j]+" Putih");
+  		}else{
+  			arrTemp.push("");
+  		}
+  	}
+  	chessboard.push(arrTemp);
+  }
   return chessboard
 }
 
 const printBoard = x => {
   // ... write your code here
+  console.log(x);
 }
 
 printBoard(makeChessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,14 +1,21 @@
 // ... write your code here
 class Spreadsheet {
   constructor () {
-    this._spreadsheet = [
+      this.spreadsheet = [
       ['Number', 'Name', 'Position', 'Points per Game']
     ]
   }
   addData (data) {
+    this.spreadsheet.push(data);
   }
   getData () {
+    console.log(this.spreadsheet);
   }
+
 }
+
+const example = new Spreadsheet()
+example.addData(['9', 'Ken Buckets', 'Point Guard', [19, 0, 11, 22, 0]])
+example.getData();
 
 module.exports = Spreadsheet
