7c2594340ed467503f2e1024bf57cfbef4bbbe3f
aldyadk <aldy.andika@gmail.com>
2017-03-29 15:36:13 +0700

    commit nested-array-1

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,66 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
-
+  let chessboard = [];
   // ... write your code here
-  return chessboard
+  var bidak1 = ['Benteng', 'Kuda', 'Gajah', 'Ratu', 'Raja', 'Gajah', 'Kuda', 'Benteng'];
+  var bidak2 = 'Pion'
+  var warna = ['Hitam','Putih'];
+  var array=[];
+
+  for(var i=0;i<bidak1.length;i++){
+    array.push(bidak1[i]+' '+warna[0]);
+  }
+
+  for(var i=0;i<8;i++){
+    array.push(bidak2+' '+warna[0]);
+  }
+
+  //dibawah ini klo mau jelaskan keterangan warna kotak yang kosong
+  /*
+  for(var x=0;x<2;i++){
+    for(var i=0;i<8;i++){
+      if (i%2===0) {
+        array.push('tidak ada bidak, hanya ada kotak kosong '+warna[1]);
+      } else {
+        array.push('tidak ada bidak, hanya ada kotak kosong '+warna[0]);
+      }
+    }
+
+    for(var i=0;i<8;i++){
+      if (i%2===0) {
+        array.push('tidak ada bidak, hanya ada kotak kosong '+warna[0]);
+      } else {
+        array.push('tidak ada bidak, hanya ada kotak kosong '+warna[1]);
+      }
+    }
+  }
+  */
+
+  for(var i=0;i<32;i++){
+    array.push('kosong');
+  }
+
+  for(var i=0;i<8;i++){
+    array.push(bidak2+' '+warna[1]);
+  }
+
+  for(var i=0;i<bidak1.length;i++){
+    array.push(bidak1[i]+' '+warna[1]);
+  }
+
+  for(var j=0;j<array.length;j+=8){
+    chessboard.push(array.slice(j,j+8));
+  }
+  return chessboard;
 }
 
 const printBoard = x => {
   // ... write your code here
+  var temp=[];
+  for(var k=0;k<8;k++){
+    console.log(x.slice(k,k+1).join('  '));
+  }
 }
 
 printBoard(makeChessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -5,10 +5,22 @@ class Spreadsheet {
       ['Number', 'Name', 'Position', 'Points per Game']
     ]
   }
+
   addData (data) {
+    this._spreadsheet.push(data);
   }
   getData () {
+    console.log(this._spreadsheet);
   }
 }
+var execute  = new Spreadsheet()
+
+execute.addData(['12', 'Joe Schmo', 'Center',[14,32,7,0,23]]);
+execute.addData(['9','Ken Buckets','Point Guard', [19,0,11,22,0]]);
+execute.addData(['31','Harvey Kay','Shooting Guard', [0,30,16,0,25]]);
+execute.addData(['18','Sally Talls','Power Forward', [18,29,26,31,19]]);
+execute.addData(['22','My DiBoux','Small Forward', [11,0,23,17,0]]);
+
+execute.getData();
 
 module.exports = Spreadsheet
