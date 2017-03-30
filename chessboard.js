d76a6d7cdd5d018bd486dbc1757b5d5e8c73a118
arahito <uarahito@yahoo.com>
2017-03-30 07:33:54 +0700

    Nested Array 1

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,47 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
+  let chessboard = [];
+  let chessPion = [];
+  let chessResult = [];
+  let chessSize = 8;
+  let chessName1 = ["Benteng", "Kuda", "Bishop", "Ratu", "Raja", "Bishop", "Kuda", "Benteng"];
+  let chessName2 = "pion";
 
   // ... write your code here
-  return chessboard
+  for (var i = 0; i < 8; i++) {
+    chessPion.push(chessName2);
+  }
+
+  for (let idxRow = 0; idxRow < chessSize; idxRow++) {
+    for (let idxCol = 0; idxCol < chessSize; idxCol++) {
+      if (idxRow === 0) {
+        chessboard.push(chessName1[idxCol]+ " Putih");
+      } else if (idxRow === 1) {
+        chessboard.push(chessPion[idxCol]+ " Putih");
+      } else if (idxRow === 6) {
+        chessboard.push(chessPion[idxCol]+ " Hitam");
+      } else if (idxRow === 7) {
+        chessboard.push(chessName1[idxCol]+ " Hitam");
+      } else {
+        chessboard.push(" ");
+      }
+    }
+  }
+
+  for (let n = 0; n < chessboard.length; n+=8) {
+    chessResult.push(chessboard.slice(n, n+8));
+  }
+  return chessResult;
 }
 
 const printBoard = x => {
-  // ... write your code here
+  // ... write your code let
+  for (let c = 0; c < 8; c++) {
+    console.log(x.slice(c, c+1).join(' '));
+  }
+
+  // console.log(x[7][0]);
 }
 
 printBoard(makeChessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -6,9 +6,28 @@ class Spreadsheet {
     ]
   }
   addData (data) {
+    // utk push ke spreadsheet
+    this._spreadsheet.push(data);
   }
+
   getData () {
+    //console log spreadsheet
+    console.log(this._spreadsheet);
   }
 }
+  let spreadsheet1 = ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]];
+  let spreadsheet2 = ["9", "Ken Buckets", "Point Guard", [19, 0, 11, 22, 0]];
+  let spreadsheet3 = ["31", "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]];
+  let spreadsheet4 = ["18", "Sally Talls", "Power Forward", [18, 29, 26, 31, 19]];
+  let spreadsheet5 = ["22", "Mo DiBoux", "Small Forward", [11, 0, 23, 17, 0]];
+
+  let result1 = new Spreadsheet();
+  result1.addData(spreadsheet1);
+  result1.addData(spreadsheet2);
+  result1.addData(spreadsheet3);
+  result1.addData(spreadsheet4);
+  result1.addData(spreadsheet5);
+  result1.getData();
+
 
 module.exports = Spreadsheet
