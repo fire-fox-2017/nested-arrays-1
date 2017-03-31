99cbbcff8db83f9a29cea952227819213aa0ee16
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-03-31 09:50:56 +0700

    fixing code

--- chessboard.js
+++ chessboard.js
@@ -1,17 +1,53 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
-
+  let chessboard = [];
   // ... write your code here
-  return chessboard
+  var color = "black white";
+  var bidak = "rook knight bishop queen king bishop knight rook";
+  var chessColor = color.split(' ');
+  var chessKingdom = bidak.split(' ');
+  var chessPion = [];
+  var i, j;
+  for (i=0; i<chessKingdom.length; i++){
+    chessPion.push("pion");
+  }
+  var chessKingdomWhite = [];
+  var chessKingdomBlack = [];
+  var pionWhite = [];
+  var pionBlack = [];
+    for(j=0; j<chessKingdom.length; j++){
+      chessKingdomWhite.push(chessColor[1] + " " + chessKingdom[j]);
+      chessKingdomBlack.push(chessColor[0] + " " + chessKingdom[j]);
+      pionWhite.push(chessColor[1] + " " + chessPion[j]);
+      pionBlack.push(chessColor[0] + " " + chessPion[j]);
+    }
+  chessboard.push(chessKingdomBlack);
+  chessboard.push(pionBlack);
+  chessboard.push(pionWhite);
+  chessboard.push(chessKingdomWhite);
+
+  var kosong = [];
+  for (var k=0; k<chessKingdom.length; k++){
+    kosong.push('kosong');
+  }
+  chessboard.splice(2, 0, kosong, kosong, kosong, kosong);
+  return chessboard;
 }
 
 const printBoard = x => {
   // ... write your code here
+  var tampil = '';
+  var baris = [];
+  for (var l=0; l<x.length; l++){
+      baris.push(x[l].join(' - '));
+  }
+  for (l=0; l<x.length; l++){
+    console.log('Baris ke '+l+' : '+baris[l]);
+  }
 }
 
-printBoard(makeChessboard())
+printBoard(makeChessboard());
 
 module.exports = {
   makeChessboard,
