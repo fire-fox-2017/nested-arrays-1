ee83483254a9ea96ac78ca87783d7af3f8d10db7
Didit Aditya <email": null>
2017-03-31 08:34:05 +0700

    Fixes the visuals

--- chessboard.js
+++ chessboard.js
@@ -7,12 +7,15 @@ const makeChessboard = () => {
   let row = 8;
   let col = 8;
 
-  let strBackRowSet = "Rook Knight Bishop King Queen Bishop Knight Rook";
+  // let strBackRowSet = "Rook Knight Bishop King Queen Bishop Knight Rook";
+  // let backRowSet = strBackRowSet.split(" ");
+
+  let strBackRowSet = "R K B K Q B K R";
   let backRowSet = strBackRowSet.split(" ");
 
   let frontRowSet = [];
   for (let i = 0; i < row; i++) {
-    frontRowSet.push("Pawn");
+    frontRowSet.push("P");
   }
 
   let other = [];
@@ -20,7 +23,9 @@ const makeChessboard = () => {
     other.push("Empty");
   }
 
-  let colors = ["White", "Black"];
+  // let colors = ["White", "Black"];
+  let colors = ["W", "B"];
+
 
   for (var i = 0; i < row; i++) {
     chessboard.push([]);
@@ -29,15 +34,15 @@ const makeChessboard = () => {
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < col; j++) {
       if (i === 0) {
-        chessboard[i].push(colors[0] + " " + backRowSet[j]);
+        chessboard[i].push(colors[0] + "" + backRowSet[j]);
       } else if (i === 1) {
-        chessboard[i].push(colors[0] + " " + frontRowSet[j]);
+        chessboard[i].push(colors[0] + "" + frontRowSet[j]);
       } else if (i === row-1) {
-        chessboard[i].push(colors[1] + " " + backRowSet[j]);
+        chessboard[i].push(colors[1] + "" + backRowSet[j]);
       } else if (i === row-2) {
-        chessboard[i].push(colors[1] + " " + frontRowSet[j]);
+        chessboard[i].push(colors[1] + "" + frontRowSet[j]);
       } else {
-        chessboard[i].push("empty");
+        chessboard[i].push("X ");
       }
     }
   }
@@ -47,11 +52,17 @@ const makeChessboard = () => {
 
 const printBoard = x => {
   // ... write your code here
-  for (let i = 0; i < x.length; i++) {
-    for (let j =0; j < x[i].length; j++) {
-      console.log(x[i][j]);
-    }
+  console.log("LEGEND:");
+  console.log("Colors: W = White, B = Black");
+  console.log("Pieces: R = Rook, K = Knight, B = Bishop, K = King, Q = Queen");
+  console.log("Other: X = Empty");
+  console.log("");
 
+  for (let i = 0; i < x.length; i++) {
+    console.log(x[i]);
+    // for (let j =0; j < x[i].length; j++) {
+    //   console.log(x[i][j]);
+    // }
   }
 }
 
