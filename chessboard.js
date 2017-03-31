69ee592de2f097d9150432a2a658da6ad9b7a3aa
oscarhermawan <franky1790@gmail.com>
2017-03-31 13:32:45 +0700

    nested-array-1-done

--- chessboard.js
+++ chessboard.js
@@ -3,12 +3,56 @@
 const makeChessboard = () => {
   let chessboard = []
 
+  let pionPutih = 'Pion Putih'
+  let pionHitam = 'Pion Hitam'
+  let rajaHitam = ['Benteng Hitam','Kuda Hitam', 'Peluncur Hitam', 'Menteri Hitam', 'Raja Hitam', 'Peluncur Hitam', 'Kuda Hitam', 'Benteng Hitam'];
+  let rajaPutih = ['Benteng Putih','Kuda Putih', 'Peluncur Putih', 'Menteri Putih', 'Raja Putih', 'Peluncur Putih', 'Kuda Putih', 'Benteng Putih'];
+
   // ... write your code here
+
+  for(let i = 0 ; i < 8 ; i++ )
+  {
+    chessboard.push([]);
+      if(i==0)
+      {
+        chessboard[i] = rajaHitam ;
+      }
+
+      else if(i==1)
+      {
+        for(let j=0;j<8;j++){
+          chessboard[i].push(pionHitam);
+        }
+      }
+
+      else if(i==6)
+      {
+        for(let j=0;j<8;j++){
+          chessboard[i].push(pionPutih);
+        }
+      }
+
+      else if(i==7)
+      {
+        chessboard[i] = rajaPutih;
+      }
+
+      else
+      {
+        for(let j=0;j<8;j++){
+          chessboard[i].push('');
+        }
+      }
+
+  }
+
+
   return chessboard
 }
 
 const printBoard = x => {
   // ... write your code here
+  console.log(x);
 }
 
 printBoard(makeChessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -6,9 +6,22 @@ class Spreadsheet {
     ]
   }
   addData (data) {
+    this._spreadsheet.push(data)
   }
+
   getData () {
+    return this._spreadsheet;
   }
 }
+
+let table = new Spreadsheet();
+let data1 = ["12", "Joe Schmo", "Center", [14,32,7,0,23]]
+let data2= ["9", "Ken Buckets", "Point Guard", [19,0,11,22,0]]
+let data3 = ["31", "Harvey Kay", "Shooting Guard", [0,30,16,0,25]]
+table.addData(data1);
+table.addData(data2);
+table.addData(data3);
+console.log(table._spreadsheet[3][2] == "Shooting Guard");
+console.log(JSON.stringify(table._spreadsheet[1][3]) == JSON.stringify([14,32,7,0,23]));
 
 module.exports = Spreadsheet
