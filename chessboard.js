5a0b64c282afe6e6737781dc881efa12130a4ca6
edim dendy &   "name": null <pair+edimdendidiwangga.zanisis@hacktiv8.com>
2017-04-01 12:46:26 +0700

    nested array 1- edi

--- chessboard.js
+++ chessboard.js
@@ -2,13 +2,51 @@
 
 const makeChessboard = () => {
   let chessboard = []
+  let anakCatur = ['Benteng', 'Kuda', 'Menteri', 'Ratu', 'Raja', 'Menteri', 'Kuda', 'Benteng'];
+  let row;
+  for (let i = 0; i < 8; i++) {
+    row = []
+    chessboard.push(row)
+    switch (i) {
+      case 0:
+        for (let k = 0; k < anakCatur.length; k++) {
+          row.push(anakCatur[k] + " Hitam")
+        }
+        break
+      case 1:
+        for (let l = 0; l < 8; l++) {
+          row.push("Pion Hitam")
+        }
+        break
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+        for (let k = 0; k < anakCatur.length; k++) {
+          row.push(0)
+        }
+        break;
+      case 6:
+        for (let l = 0; l < 8; l++) {
+          row.push("Pion Putih")
+        }
+        break
+      case 7:
+        for (let k = 0; k < anakCatur.length; k++) {
+          row.push(anakCatur[k] + " Putih")
+        }
+        break
+      default:
+        console.log('default');
+        
+    }
+  }
 
-  // ... write your code here
   return chessboard
 }
 
 const printBoard = x => {
-  // ... write your code here
+  console.log(x)
 }
 
 printBoard(makeChessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -2,13 +2,31 @@
 class Spreadsheet {
   constructor () {
     this._spreadsheet = [
-      ['Number', 'Name', 'Position', 'Points per Game']
+      	['Number', 'Name', 'Position', 'Points per Game'],
+		["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
+		["9", "Ken Buckets", "Point Guard", [19,0,11,22,0]],
+		["31", "Harvey Kay", "Shooting Guard", [0,30,16,0,25]],
+		["18", "Sally Talls", "Power Forward", [18,29,26,31,19]],
+		["22", "Mo DiBoux", "Small Forward", [11,0,23,17,0]]
     ]
   }
   addData (data) {
-  }
-  getData () {
+  	this._spreadsheet.push(data)
   }
 }
 
+let spreed = new Spreadsheet()
+
+let data = ["1", "John Doe", "Director", [10,10,10,10]];
+
+spreed.addData(data)
+console.log(spreed._spreadsheet)
+console.log(spreed._spreadsheet[3][2] == "Shooting Guard");
+true
+
+console.log(JSON.stringify(spreed._spreadsheet[1][3]) == JSON.stringify([14, 32, 7, 0, 23]));
+true
+
 module.exports = Spreadsheet
+
+
