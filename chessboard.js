a952bbb8b78ffe9f736924cffae31fe0f5ac5800
jamessraun <jeezybig@gmail.com>
2017-04-03 10:46:01 +0700

    first commit

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,40 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
-
+let chessboard= [];
   // ... write your code here
-  return chessboard
-}
+let susunanPutih=[];
+let susunanHitam=[];
+let bidakHitam=['bentang Hitam','kuda Hitam','peluncur Hitam','raja Hitam','ratu Hitam','peluncur Hitam','kuda Hitam','benteng Hitam','pion Hitam','pion Hitam','pion Hitam','pion Hitam','pion Hitam','pion Hitam','pion Hitam','pion Hitam'];
+let bidakPutih=['bentang Putih','kuda Putih','peluncur Putih','raja Putih','ratu Putih','peluncur Putih','kuda Putih','benteng Putih','pion Putih','pion Putih','pion Putih','pion Putih','pion Putih','pion Putih','pion Putih','pion Putih'];
+let kosong=['','','','','','','',''];
+  for(let i=0;i<8;i++)
+  {
+    if(i===0){
+        chessboard.push(bidakHitam.slice(0,8));
+    }
+    else if(i===1){
+       chessboard.push(bidakHitam.slice(8,16));
+    }
+    else if(i===6)
+    {
+      chessboard.push(bidakPutih.slice(8,16));
+    }
+    else if(i===7)
+    {
+      chessboard.push(bidakPutih.slice(0,8));
+    }
+    else {
+      chessboard.push(kosong);
+    }
+  }
 
+  return chessboard;
+}
+makeChessboard();
 const printBoard = x => {
   // ... write your code here
+  console.log(x);
 }
 
 printBoard(makeChessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,14 +1,21 @@
 // ... write your code here
 class Spreadsheet {
-  constructor () {
+  constructor (data) {
     this._spreadsheet = [
       ['Number', 'Name', 'Position', 'Points per Game']
     ]
   }
   addData (data) {
+
+    this._spreadsheet.push(data);
   }
   getData () {
+    console.log(this._spreadsheet);
   }
 }
 
+var laporan = new Spreadsheet;
+
+laporan.addData(['01', 'james', 'center', '50']);
+laporan.getData();
 module.exports = Spreadsheet
