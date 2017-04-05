f50b8a9ee6a9f8331e93ad475578cd25f33021ce
Ambo Dalle <ambodalle@Ambos-MacBook-Pro.local>
2017-04-05 10:57:38 +0700

    revisi

--- chessboard.js
+++ chessboard.js
@@ -4,25 +4,13 @@ const makeChessboard = () => {
 
     let chessboard = [];
     let data = 8;
+    let chess_hitam = ['Benteng','Kuda' ,'Bishop' ,'Raja' ,'Mentri' ,'Bishop' , 'Kuda' ,'Benteng' ]
+
     for (let i=0; i<data; i++){
       chessboard.push([])
         for(let j=0; j<data; j++) {
-            if(i == 0 && j == 0) {
-              chessboard[i].push("Benteng Hitam");
-            } else if(i==0 && j==1) {
-              chessboard[i].push("Kuda Hitam");
-            } else if(i==0 && j==2) {
-              chessboard[i].push("Bishop Hitam");
-            } else if(i==0 && j==3) {
-              chessboard[i].push("Raja Hitam");
-            } else if(i==0 && j==4) {
-              chessboard[i].push("Mentri Hitam");
-            } else if(i==0 && j==5) {
-              chessboard[i].push("Bishop Hitam");
-            } else if(i==0 && j==6) {
-              chessboard[i].push("Kuda Hitam");
-            } else if(i==0 && j==7) {
-              chessboard[i].push("Benteng Hitam");
+            if(i==0){
+              chessboard[i].push(`${chess_hitam[j]} Hitam`)
             }
               //batas if 1
             if(i == 1) {
@@ -35,25 +23,9 @@ const makeChessboard = () => {
               chessboard[i].push("Pion Putih");
               }
               //batas if 2
-
-              if(i == 7 && j == 0) {
-                chessboard[i].push("Benteng Putih");
-              } else if(i==7 && j==1) {
-                chessboard[i].push("Kuda Putih");
-              } else if(i==7 && j==2) {
-                chessboard[i].push("Bishop Putih");
-              } else if(i==7 && j==3) {
-                chessboard[i].push("Raja Putih");
-              } else if(i==7 && j==4) {
-                chessboard[i].push("Mentri Putih");
-              } else if(i==7 && j==5) {
-                chessboard[i].push("Bishop Putih");
-              } else if(i==7 && j==6) {
-                chessboard[i].push("Kuda Putih");
-              } else if(i==7 && j==7) {
-                chessboard[i].push("Benteng Putih");
+              if(i==7){
+                chessboard[i].push(`${chess_hitam[j]} Putih`)
               }
-
                   //Batas If 2
         }
     }
